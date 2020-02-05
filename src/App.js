221891b06d2a4b9077e868005ab327a925cf088a
SihamBen <siham.bendaia37@gmail.com>
2020-02-05 18:39:08 +0100

    added infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import Repos from "./Repos";
+import RepoItem from "./RepoItem";
+import InfiniteScroll from"react-infinite-scroll-component";
+import Loading from './Loading';
 class  App extends Component {
   state={
     reposList:[],
-    loading:true,
+    hasMore:true,
+    page:2
   }
-componentDidMount()
+  componentDidMount()
 {
   axios.get('https://api.github.com/search/repositories?q=created:>2020-01-04&sort=stars&order=desc').then(
-    res=>{this.setState({reposList:res.data.items,loading:false})
+    res=>{(res.data.items.length===0)?this.setState({hasMore:false}):this.setState({reposList:res.data.items})}
+  )
+}
+  fetchMoreData =()=>
+{const url='https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${this.state.page}'
+  axios.get(url).then(
+    res=>{this.setState({
+      reposList: this.state.reposList.concat(res.data.items),page:this.state.page+1
+    })
   console.log(res.data.items)}
   )
 }
   render() { 
     return (
       <div>
-        
-        <Repos loading={this.state.loading} reposList={this.state.reposList}></Repos>
-      </div>
+
+         <h1 style={{textAlign:"center",color:"rgb(79, 79, 80)"}}>Trending repo on GitHub</h1>
+         <div style={{marginLeft:"25%",marginRight:"25%"}}>
+        <InfiniteScroll
+          dataLength={this.state.reposList.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.hasMore}
+          loader={<Loading></Loading>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {this.state.reposList.map(repo=>
+            (<RepoItem repo={repo} key={repo.id}></RepoItem>))}
+        </InfiniteScroll>
+      </div></div>
     ) ;
   }
 }
--- src/repoItem.js
+++ src/repoItem.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './repoItem.css';
  import PropTypes from "prop-types";
-const RepoItem = ({repo}) => {
+const RepoItem = (props) => {
     return (
     <div className="repoItem">
-    <div className="avatar"><img alt='' src={repo.owner.avatar_url}></img></div>
-    <div className="infos"><h3>{repo.name}</h3>
-           <p>{(repo.description)?repo.description:"No description provided"}</p>
-           <p>
-           <span className="stars">Stars: {repo.stargazers_count}</span>
-           <span className="issues">Issues: {repo.open_issues_count} </span>
-           <span className="owner">{repo.created_at} By {repo.owner.login}</span></p>
+    <div className="avatar"><img alt='' src={props.repo.owner.avatar_url}></img></div>
+    <div className="infos"><h3>{props.repo.name}</h3>
+           <p>{(props.repo.description===null)?"No description provided":props.repo.description}</p>
+           <p><span className="issues">issues :{props.repo.open_issues_count} </span>
+           <span className="stars">stars:{props.repo.stargazers_count}</span>
+           <span>{props.repo.created_at} by {props.repo.owner.login}</span></p>
 </div>
         </div>
     );
